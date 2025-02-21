4a0996680b2419d7419f00daef722613082b720b
Wilfran <wpajaro78@gmail.com>
2025-02-21 16:11:51 -0500

    Segundo Commit

--- rango/views.py
+++ rango/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 
 def index(request):
-    return HttpResponse("Rango dice ¡Hola compañero!")
+    return HttpResponse("Rango dice ¡Hola compañero! <a href='/rango/about/'> Ir a About</a>")
+
+def about(request):
+    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Volver al Inicio</a>")
 
